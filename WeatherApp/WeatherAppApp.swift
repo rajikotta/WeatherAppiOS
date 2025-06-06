0c51174bef9ae334838a692b3a6e499914e56b50
Raji Chithrangathan <raji.chithrangathan@goldenscent.com>
2025-06-06 11:10:16 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Raji on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
